d3dc361c02759320605c20947d073bf69d56a668
Renato Ganske Junior <renatoganske.jr@gmail.com>
2024-08-15 23:39:58 -0300

    Implementado método Update.

--- src/main/java/com/renatoganske/arquitetura_hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/renatoganske/arquitetura_hexagonal/adapters/in/controller/CustomerController.java
@@ -5,7 +5,9 @@ import com.renatoganske.arquitetura_hexagonal.adapters.in.controller.request.Cus
 import com.renatoganske.arquitetura_hexagonal.adapters.in.controller.response.CustomerResponse;
 import com.renatoganske.arquitetura_hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.renatoganske.arquitetura_hexagonal.application.ports.in.InsertCustomerInputPort;
+import com.renatoganske.arquitetura_hexagonal.application.ports.in.UpdateCustomerInputPort;
 import jakarta.validation.Valid;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,9 @@ public class CustomerController {
 
     @Autowired
     private FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
 
     @Autowired
     private CustomerMapper customerMapper;
@@ -35,6 +40,15 @@ public class CustomerController {
         var customer = findCustomerByIdInputPort.find(id);
         var customerResponse = customerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok(customerResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id,
+                                       @Valid @RequestBody CustomerRequest customerRequest) {
+        var customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
     }
 
 }
